015111da5dcdac006efa6851b742f61e5b37428a
abdulwariskhan <hellokhan.ch@gmail.com>
2024-04-14 00:27:07 +0200

    Design the CardView

--- SwiftUI Fructus/View/FruitCardView.swift
+++ SwiftUI Fructus/View/FruitCardView.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct FruitCardView: View {
+    
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       
+        ZStack {
+            VStack(spacing: 20) {
+                // FRUIT: IMAGE
+                Image(.blueberry)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                // FRUIT: TITLE
+                Text("Blueberry")
+                    .foregroundStyle(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0 , opacity: 0.15), radius: 2, x: 2, y: 2)
+                // FRUIT: HEADLINE
+                // BUTTON: START
+            } //: VSTACK
+            
+        } //: ZSTACK
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberry"), Color("ColorBlueberryDark")]), startPoint: .bottom, endPoint:.top))
+        .cornerRadius(20)
+     
     }
 }
 
+// MARK: - PREVIEW
 #Preview {
     FruitCardView()
+        .previewLayout(.fixed(width: 320, height: 640))
 }

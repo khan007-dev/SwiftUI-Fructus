2b5bf51a1edbeb6d268c3d90dd6dd8492cd7526e
abdulwariskhan <hellokhan.ch@gmail.com>
2024-04-13 08:39:41 +0200

    Initial Commit

---
+++ SwiftUI Fructus/SwiftUI_FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FructusApp.swift
+//  SwiftUI Fructus
+//
+//  Created by Khan on 13.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

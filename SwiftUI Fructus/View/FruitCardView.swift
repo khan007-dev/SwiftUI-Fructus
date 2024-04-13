5753999cc8453f1d73c94d4ab8956286c7eb4d46
abdulwariskhan <hellokhan.ch@gmail.com>
2024-04-14 00:26:54 +0200

    Design the CardView

---
+++ SwiftUI Fructus/View/FruitCardView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitCardView.swift
+//  SwiftUI Fructus
+//
+//  Created by Khan on 13.04.2024.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitCardView()
+}

95f21e04d4913ccf659b594985e227b499289cea
abdulwariskhan <hellokhan.ch@gmail.com>
2024-04-14 16:23:37 +0200

    added Data and Model

---
+++ SwiftUI Fructus/Data/FruitData.swift
@@ -0,0 +1,8 @@
+//
+//  FruitData.swift
+//  SwiftUI Fructus
+//
+//  Created by Khan on 14.04.2024.
+//
+
+import Foundation
---
+++ SwiftUI Fructus/Model/FruitModel.swift
@@ -0,0 +1,8 @@
+//
+//  FruitModel.swift
+//  SwiftUI Fructus
+//
+//  Created by Khan on 14.04.2024.
+//
+
+import Foundation

ca310af8447257a248d9624d3d613a43bd04e417
abdulwariskhan <hellokhan.ch@gmail.com>
2024-04-15 20:49:25 +0200

    created list view

---
+++ SwiftUI Fructus/View/FruitRowView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitRowView.swift
+//  SwiftUI Fructus
+//
+//  Created by Khan on 15.04.2024.
+//
+
+import SwiftUI
+
+struct FruitRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitRowView()
+}

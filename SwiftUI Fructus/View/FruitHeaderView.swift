626c3465daa0eecb9c65a74af61c00b16b43a918
abdulwariskhan <hellokhan.ch@gmail.com>
2024-04-17 17:05:26 +0200

    complete FruitDetialView

---
+++ SwiftUI Fructus/View/FruitHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitHeaderView.swift
+//  SwiftUI Fructus
+//
+//  Created by Khan on 17.04.2024.
+//
+
+import SwiftUI
+
+struct FruitHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitHeaderView()
+}
---
+++ SwiftUI Fructus/View/SourceLinkView.swift
@@ -0,0 +1,18 @@
+//
+//  SourceLinkView.swift
+//  SwiftUI Fructus
+//
+//  Created by Khan on 17.04.2024.
+//
+
+import SwiftUI
+
+struct SourceLinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SourceLinkView()
+}

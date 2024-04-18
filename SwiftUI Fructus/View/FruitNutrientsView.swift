b09bb62a5a515727eaacccd4547104377ec1e07f
abdulwariskhan <hellokhan.ch@gmail.com>
2024-04-18 16:49:58 +0200

    Implemented Fruit NutrientsView

---
+++ SwiftUI Fructus/View/FruitNutrientsView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitNutrientsView.swift
+//  SwiftUI Fructus
+//
+//  Created by Khan on 18.04.2024.
+//
+
+import SwiftUI
+
+struct FruitNutrientsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitNutrientsView()
+}

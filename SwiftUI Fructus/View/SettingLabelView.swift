a67ce5a39bdba171946c4d97ff6634a429c8e53c
abdulwariskhan <hellokhan.ch@gmail.com>
2024-04-22 08:49:04 +0200

    completed Setting LabelView

---
+++ SwiftUI Fructus/View/SettingLabelView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingLabelView.swift
+//  SwiftUI Fructus
+//
+//  Created by Khan on 22.04.2024.
+//
+
+import SwiftUI
+
+struct SettingLabelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingLabelView()
+}
